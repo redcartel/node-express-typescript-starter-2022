625401748a5c75c0f3b24518506349cf76ad341a
Carter Adams <carteradams@protonmail.com>
2022-04-13 23:45:12 -0400

    jest import issue fixed

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,9 @@ import { pathsToModuleNameMapper } from 'ts-jest/utils';
 
 const config = {
     preset: "ts-jest",
-    testEnvironment: "node",
-    moduleDirectories: ["node_modules", "."],
-    roots: ["./"],
     modulePathIgnorePatterns: ["<rootDir>/dist/"],
     moduleNameMapper: {
-        'src/(.*)$': '<rootDir>/src/$1'
+        '^src/(.*)$': '<rootDir>/src/$1'
     }
 }
 
