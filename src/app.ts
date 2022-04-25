1f06c12379198e68e5556bf507c75d97c984b090
Carter Adams <carteradams@protonmail.com>
2022-04-25 13:50:05 -0400

    added urlencoded middleware by default

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const app = express()
 
 // Apply most middleware first
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(helmet())
 app.use(morgan('tiny'))
