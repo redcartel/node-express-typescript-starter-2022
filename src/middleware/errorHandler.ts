6c517d04276f648f099b2f194f9a6c7ea49b40bd
Carter Adams <carteradams@protonmail.com>
2023-08-26 15:15:33 -0400

    cookie-parser in app

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -4,7 +4,7 @@ import config from '../config'
 /**
  * 500 response & log when errors are raised.
  */
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
+const errorHandler: ErrorRequestHandler = (err, _req, res) => {
     console.error(err);
     return res.status(500).json({
         message: config.nodeEnv === 'production' ?
